ce6fc8ad6c0cdec4dc1f6f2c456b022a46a37118
omarelouahdani <omarelouahdani7@gmail.com>
2024-10-10 16:51:40 +0200

    last refactoring

--- src/main/java/org/sid/sgbankbackend/SgBankBackendApplication.java
+++ src/main/java/org/sid/sgbankbackend/SgBankBackendApplication.java
@@ -1,11 +1,18 @@
 package org.sid.sgbankbackend;
 
 
-import org.sid.sgbankbackend.service.AccountService;
+import org.sid.sgbankbackend.initialization.MemoryDataInitializer;
+import org.sid.sgbankbackend.model.AccountOperation;
+import org.sid.sgbankbackend.repositories.AccountOperationRepository;
+import org.sid.sgbankbackend.repositories.AccountRepository;
+import org.sid.sgbankbackend.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @SpringBootApplication
@@ -16,9 +23,27 @@ public class SgBankBackendApplication {
     }
 
     @Bean
-    CommandLineRunner start(AccountService accountService) {
+    CommandLineRunner start(CustomerRepository customerRepository,
+                            AccountRepository accountRepository,
+                            AccountOperationRepository accountOperationRepository) {
         return args -> {
-            accountService.initData();
+            MemoryDataInitializer initializer = new MemoryDataInitializer();
+            // Initialize customers and accounts
+            var customers = initializer.initializeCustomers();
+            var accounts = initializer.initializeAccounts(customers);
+
+            // Save customers to the database
+            customerRepository.saveAll(customers);
+
+            // Save accounts and account operations to the database
+            accounts.forEach(account -> {
+                accountRepository.save(account);
+                // Initialize and save account operations
+                List<AccountOperation> accountOperations = new ArrayList<>();
+                initializer.initializeAccountOperations(account, accountOperations);
+                accountOperationRepository.saveAll(accountOperations);
+
+            });
         };
     }
 
